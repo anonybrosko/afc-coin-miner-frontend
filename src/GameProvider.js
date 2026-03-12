cd316f0ee46d480605a7ab34f10e2ad0e2cd7096
anonybrosko <anonybrosko@gmail.com>
2026-03-11 22:08:04 -0700

    Fixed Prestige Debug Reset

--- src/GameProvider.js
+++ src/GameProvider.js
@@ -250,7 +250,13 @@ export const GameProvider = ({ children }) => {
       <button
         onClick={() => {
           localStorage.removeItem("afcGame");
-          window.location.reload();
+          setBalance(0);
+          setClicks(0);
+          setUpgrades({});
+          setPsuStates({});
+          setActivePsu(null);
+          setPrestige(0);
+          setActiveNetworks([]);
         }}
       >
         Reset Game
