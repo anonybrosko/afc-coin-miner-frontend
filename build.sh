210db092a3f53e5d6dc0c888a276d76dc258e314
anonybrosko <anonybrosko@gmail.com>
2026-03-12 00:50:30 -0700

    Updated #rcss

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
-npm run build
-
-if git add .; then
-  if git commit -m "${1}"; then
-    git push
+if npm run build; then
+  if npm run deploy; then
+    if git add .; then
+      if git commit -m "${1}"; then
+        git push
+      fi
+    fi
   fi
 fi
